49d82cedaade069754d57e3cdead92122d9c9069
kanini-esther <esther.njeri.kanini@gmail.com>
2022-05-11 15:11:34 +0300

    database questions added

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello you.")
+    return HttpResponse("Hello you,you're at the polls index")
